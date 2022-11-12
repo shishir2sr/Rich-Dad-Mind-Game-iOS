28da6fb0069451092d851f6c3a0b5e9c52e21dfa
Yasir Arefin Tusher <shishir2sr@users.noreply.github.com>
2022-11-12 09:32:24 +0600

    Initial Commit

---
+++ Rich Dad Poor Dad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rich Dad Poor Dad
+//
+//  Created by Yeasir Arefin Tusher on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
